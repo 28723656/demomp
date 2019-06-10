a035e1105d4bdf614c338df79dd4dbc7ed1aacbe
xiarui <28723656@qq.com>
2019-06-10 18:41:07 +0800

    add:nice

--- src/main/java/com/java/demomp/sys/service/impl/PersonServiceImpl.java
+++ src/main/java/com/java/demomp/sys/service/impl/PersonServiceImpl.java
@@ -1,10 +1,15 @@
 package com.java.demomp.sys.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.java.demomp.sys.entity.Person;
 import com.java.demomp.sys.mapper.PersonMapper;
 import com.java.demomp.sys.service.PersonService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.sql.Wrapper;
+import java.util.List;
 
 /**
  * <p>
@@ -22,6 +27,13 @@ public class PersonServiceImpl extends ServiceImpl<PersonMapper, Person> impleme
         person.setUserName("xia");
         person.setPassword("oo");
         boolean insert = person.insert();
+
+        QueryWrapper<Person> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id",1);
+        List<Person> people = baseMapper.selectList(queryWrapper);
+        Person person1 = people.get(0);
+        System.out.println(person1);
+
         if(insert){
             System.out.println(person);
             return person;
