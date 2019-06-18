857b1eb62970b0104cf1011133428d355b832329
xiarui <28723656@qq.com>
2019-06-18 18:46:44 +0800

    add:完成计划的时候百分比显示部分完成

--- src/main/java/com/java/demomp/plan/service/impl/PlanServiceImpl.java
+++ src/main/java/com/java/demomp/plan/service/impl/PlanServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author lost丶wind
@@ -26,60 +26,52 @@ public class PlanServiceImpl extends ServiceImpl<PlanMapper, Plan> implements Pl
 
     /**
      * 添加计划
+     *
      * @param plan
      * @return
      */
     public Integer addPlan(Plan plan) {
-       return baseMapper.insert(plan);
+        return baseMapper.insert(plan);
 
     }
 
     public List<Plan> getPlanByType(Integer type) {
-        return baseMapper.selectList(new QueryWrapper<Plan>().eq("type",type).orderByDesc("top",    "rank"));
+        return baseMapper.selectList(new QueryWrapper<Plan>().eq("type", type).orderByDesc("top", "rank"));
     }
 
     public Integer updatePlanFinishedById(Plan plan) {
-        int finished = plan.getFinished() == 1?0:1;
-
-        //一、 更新自己的状态
+        int finished = plan.getFinished() == 1 ? 0 : 1;
+        //1、 更新自己的状态
         int updateNum = baseMapper.update(new Plan().setFinished(finished).setPercent(100.00), new UpdateWrapper<Plan>().eq("id", plan.getId()));
 
-
-        // 二、更新周计划
-
-           // 1.通过id获取父亲下面的节点的个数
-        Integer parentId = plan.getParentId();
-            // 2.通过父节点获取完成的数量和总数量
-        Integer countTotal = baseMapper.selectCount(new QueryWrapper<Plan>().eq("parent_id", parentId));
-        Integer countFinished = baseMapper.selectCount(new QueryWrapper<Plan>().eq("parent_id", parentId).eq("finished", 1));
-          // 3.更新父亲百分比
-        Double percent = countFinished * 1.00 / countTotal * 100;
-        baseMapper.update(new Plan().setPercent(percent), new QueryWrapper<Plan>().eq("id", parentId));
-
-        // 三、更新月计划
-        // 1.通过id获取父亲下面的节点的个数
-        Plan weekPlan= baseMapper.selectById(parentId);
-        Integer weekPlanParentId = weekPlan.getParentId();
-        // 2.通过父节点获取完成的数量和总数量
-        Integer weekCountTotal = baseMapper.selectCount(new QueryWrapper<Plan>().eq("parent_id", weekPlanParentId));
-        Double weekPercentFinished = baseMapper.selectSumPercent(weekPlanParentId); // 选出百分比
-        // 3.更新父亲百分比
-        Double weekPercent = weekPercentFinished * 1.00 / weekCountTotal;
-        baseMapper.update(new Plan().setPercent(weekPercent), new QueryWrapper<Plan>().eq("id", weekPlanParentId));
-
-
-        // 三、更新年计划
-        // 1.通过id获取父亲下面的节点的个数
-        Plan yearPlan= baseMapper.selectById(weekPlanParentId);
-        Integer yearPlanParentId = yearPlan.getParentId();
-        // 2.通过父节点获取完成的数量和总数量
-        Integer yearCountTotal = baseMapper.selectCount(new QueryWrapper<Plan>().eq("parent_id", yearPlanParentId));
-        Double yearPercentFinished = baseMapper.selectSumPercent(yearPlanParentId); // 选出百分比
-        // 3.更新父亲百分比
-        Double yearPercent = yearPercentFinished * 1.00 / yearCountTotal;
-        baseMapper.update(new Plan().setPercent(yearPercent), new QueryWrapper<Plan>().eq("id", yearPlanParentId));
-
-
+        // 2、更新周计划
+        Integer weekParentId = updateFatherByParentId(plan.getParentId());
+        // 3、更新月计划
+        Integer monthParentId = updateFatherByParentId(weekParentId);
+        // 4、更新年计划
+        updateFatherByParentId(monthParentId);
         return updateNum;
+    }
+
+
+    // 抽取一个公共发放，通过父id更新父亲的百分比状态并 返回那个父亲的父id
+
+    /**
+     * 传入父id
+     *
+     * @param parentId
+     * @return
+     */
+    public Integer updateFatherByParentId(Integer parentId) {
+        // 1.通过父id获取父亲实体
+        Plan plan = baseMapper.selectById(parentId);
+        Integer planParentId = plan.getParentId();
+        // 2.通过父节点获取完成的数量和总数量
+        Integer countTotal = baseMapper.selectCount(new QueryWrapper<Plan>().eq("parent_id", planParentId));
+        Double percentFinished = baseMapper.selectSumPercent(planParentId); // 选出百分比
+        // 3.更新父亲百分比
+        Double percent = percentFinished * 1.00 / countTotal;
+        baseMapper.update(new Plan().setPercent(percent), new QueryWrapper<Plan>().eq("id", planParentId));
+        return planParentId;
     }
 }
